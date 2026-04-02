60349536041cc525039330ba00789fe117386838
lupus <lupusaesticus@gmail.com>
2026-04-02 14:18:32 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+VERSION="1.1.0"
+
+ROOT_PATH=$PWD
+SRC_DIR="plugin.video.lasteekraan.err.ee"
 ADDON_ID="plugin.video.lasteekraan.err.ee"
-VERSION="1.0.0"
 DIST_DIR="dist"
+REPO_DIR="$ROOT_PATH/../repository.kodi.lupus/zips/$ADDON_ID"
 
 echo "Cleaning old builds..."
 rm -rf $DIST_DIR
 mkdir -p $DIST_DIR/$ADDON_ID
 
-echo "Copying core files..."
-cp addon.xml default.py lasteekraan_addon.py $DIST_DIR/$ADDON_ID/
+echo "Copying core files from $SRC_DIR..."
+cp $SRC_DIR/addon.xml $SRC_DIR/lasteekraan_addon.py $DIST_DIR/$ADDON_ID/
 
 echo "Copying resources..."
-# This copies the entire resources folder (including lib/ and language/)
-cp -r resources $DIST_DIR/$ADDON_ID/
+cp -r $SRC_DIR/resources $DIST_DIR/$ADDON_ID/ll
 
 # Remove any python cache files that might have been copied
 find $DIST_DIR -name "__pycache__" -type d -exec rm -rf {} +
 
 echo "Creating ZIP package..."
 cd $DIST_DIR
-zip -r ../${ADDON_ID}-${VERSION}.zip $ADDON_ID
-cd ..
+# Create the versioned ZIP for the Kodi Repo
+mkdir -p "$REPO_DIR"
+zip -r "$REPO_DIR/${ADDON_ID}-${VERSION}.zip" "$ADDON_ID"
+# Create the zip for GitHub Release
+zip -r "$ROOT_PATH/${ADDON_ID}.zip" "$ADDON_ID"
+
+# Cleanup
+cd "$ROOT_PATH"
+rm -rf $DIST_DIR
 
 echo "------------------------------------------------"
 echo "Build complete: ${ADDON_ID}-${VERSION}.zip"
