d1b19c247e764c7f19a1f5e511288c562c5db555
lupus <lupusaesticus@gmail.com>
2026-04-02 14:30:09 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -16,18 +16,23 @@ echo "Copying core files from $SRC_DIR..."
 cp $SRC_DIR/addon.xml $SRC_DIR/lasteekraan_addon.py $DIST_DIR/$ADDON_ID/
 
 echo "Copying resources..."
-cp -r $SRC_DIR/resources $DIST_DIR/$ADDON_ID/ll
+cp -r $SRC_DIR/resources $DIST_DIR/$ADDON_ID/
 
 # Remove any python cache files that might have been copied
 find $DIST_DIR -name "__pycache__" -type d -exec rm -rf {} +
 
 echo "Creating ZIP package..."
 cd $DIST_DIR
-# Create the versioned ZIP for the Kodi Repo
-mkdir -p "$REPO_DIR"
-zip -r "$REPO_DIR/${ADDON_ID}-${VERSION}.zip" "$ADDON_ID"
+
 # Create the zip for GitHub Release
 zip -r "$ROOT_PATH/${ADDON_ID}.zip" "$ADDON_ID"
+
+echo "Updating Repo Store..."
+mkdir -p "$REPO_DIR"
+# Copy the addon.xml to the repo destination
+cp "$ADDON_ID/addon.xml" "$REPO_DIR/"
+# Create the versioned ZIP for the Kodi Repo
+zip -r "$REPO_DIR/${ADDON_ID}-${VERSION}.zip" "$ADDON_ID"
 
 # Cleanup
 cd "$ROOT_PATH"
